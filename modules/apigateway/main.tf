fac0985e59066703e35aab96fcd5cc2cead08a53
Dennis Amankwa <amankwa32@hotmail.com>
2025-12-14 23:49:47 +0000

    Create main.tf

---
+++ modules/apigateway/main.tf
@@ -0,0 +1,29 @@
+resource "aws_apigatewayv2_api" "this" {
+  name          = "${var.env}-health-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.this.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.lambda_invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "health" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "GET /health"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "api" {
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
